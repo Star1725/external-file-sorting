4d9dda572da800da3f41ee5e21a76838bd412ca6
Star1725 <65925726+Star1725@users.noreply.github.com>
2025-02-01 12:39:04 +0300

    implemented the method splitSortedFile

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Generator generator = new Generator();
+        Sorter sorter = new Sorter();
+        try {
+            File file = generator.generate("Numbs", 100);
+            sorter.sortFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,13 +1,15 @@
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Sorter {
-    private static int MAX_CHUNK_SIZE = 100000;//максимальный размер чанка, который помещается в память
+    private static int MAX_CHUNK_SIZE = 10;//максимальный размер чанка, который помещается в память
     private static final int COUNT_THREADS = Runtime.getRuntime().availableProcessors();
 
 
-    public File sortFile(File file){
+    public File sortFile(File file) throws IOException {
         //деление файла на части с параллельной сортировкой
         List<File> sortedChunks = splitSortedFile(file);
 
@@ -16,10 +18,32 @@ public class Sorter {
     }
 
 
-    private List<File> splitSortedFile(File file) {
+    private List<File> splitSortedFile(File file) throws IOException {
 
-        List<File> sortedChunks = new ArrayList<File>();
+        List<File> sortedChunks = new ArrayList<>();
+        try(Scanner scanner = new Scanner(new FileInputStream(file))){
+            List<Long> chunk = new ArrayList<>();
+            while(scanner.hasNextLong()){
+                chunk.clear();
 
+                //читаем в чанк
+                while (chunk.size() < MAX_CHUNK_SIZE && scanner.hasNextLong()){
+                    chunk.add(scanner.nextLong());
+                }
+
+                //сортируем чанк
+                Collections.sort(chunk);
+
+                //записываем чанк в новый файл
+                File chunkFile = File.createTempFile("sortedChunk", ".txt");
+                try(PrintWriter writer = new PrintWriter(new FileWriter(chunkFile))){
+                    for(Long numberInChunk : chunk){
+                        writer.println(numberInChunk);
+                    }
+                }
+                sortedChunks.add(chunkFile);
+            }
+        }
         return sortedChunks;
     }
 
